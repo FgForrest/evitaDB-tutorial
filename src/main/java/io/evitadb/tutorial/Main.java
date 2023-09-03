545b98ccc679fcc139a4e1470636b12ec162a253
Jan Novotný <novotnaci@gmail.com>
2023-09-03 19:55:00 +0200

    feat: creating ad hoc schema

--- src/main/java/io/evitadb/tutorial/Main.java
+++ src/main/java/io/evitadb/tutorial/Main.java
@@ -1,10 +1,13 @@
 package io.evitadb.tutorial;
 
 import io.evitadb.api.EvitaContract;
+import io.evitadb.api.requestResponse.schema.Cardinality;
 import io.evitadb.driver.EvitaClient;
 import io.evitadb.driver.config.EvitaClientConfiguration;
 
-import java.util.Set;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Locale;
 
 /**
  * This example class shows how to create evitaDB client and connect to the server.
@@ -22,19 +25,62 @@ public class Main {
                         .build()
         );
 
-        System.out.println("evitaDB connected ... creating new catalog `evita-tutorial`:");
+        System.out.println("evitaDB connected ... defining ad-doc schema:");
+        // clear existing catalog to start with fresh new one
+        evita.deleteCatalogIfExists("evita-tutorial");
+        // define new catalog
         evita.defineCatalog("evita-tutorial")
                 .withDescription("This is a tutorial catalog.")
                 .updateViaNewSession(evita);
-        System.out.println("Catalog `evita-tutorial` created, listing all catalogs:");
-        final Set<String> catalogNames = evita.getCatalogNames();
-        if (catalogNames.isEmpty()) {
-            System.out.println("\t- No catalogs found");
-        } else {
-            for (String catalogName : catalogNames) {
-                System.out.println("\t- Catalog: " + catalogName);
-            }
-        }
+        // define a new product
+        evita.updateCatalog(
+                "evita-tutorial",
+                session -> {
+                    // let's create a Lenovo brand first
+                    session.createNewEntity("Brand", 1)
+                            .setAttribute("name", Locale.ENGLISH, "Lenovo")
+                            .upsertVia(session);
+
+                    // now create an example category tree
+                    session.createNewEntity("Category", 10)
+                            .setAttribute("name", Locale.ENGLISH, "Electronics")
+                            .upsertVia(session);
+
+                    session.createNewEntity("Category", 11)
+                            .setAttribute("name", Locale.ENGLISH, "Laptops")
+                            // laptops will be a child category of electronics
+                            .setParent(10)
+                            .upsertVia(session);
+
+                    // finally, create a product
+                    session.createNewEntity("Product")
+                            // with a few attributes
+                            .setAttribute("name", Locale.ENGLISH, "ThinkPad P15 Gen 1")
+                            .setAttribute("cores", 8)
+                            .setAttribute("graphics", "NVIDIA Quadro RTX 4000 with Max-Q Design")
+                            // and price for sale
+                            .setPrice(
+                                    1, "basic",
+                                    Currency.getInstance("USD"),
+                                    new BigDecimal("1420"), new BigDecimal("20"), new BigDecimal("1704"),
+                                    true
+                            )
+                            // link it to the manufacturer
+                            .setReference(
+                                    "brand", "Brand",
+                                    Cardinality.EXACTLY_ONE,
+                                    1
+                            )
+                            // and to the laptop category
+                            .setReference(
+                                    "categories", "Category",
+                                    Cardinality.ZERO_OR_MORE,
+                                    11
+                            )
+                            .upsertVia(session);
+                }
+        );
+        // close the connection
         evita.close();
         System.out.println("evitaDB connection closed");
     }
