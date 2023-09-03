bb1f80533db31f7ff9e0b3e9a6d865ba8f8b9071
Jan Novotný <novotnaci@gmail.com>
2023-09-03 20:51:24 +0200

    feat: creating schema upfront

--- src/main/java/io/evitadb/tutorial/Main.java
+++ src/main/java/io/evitadb/tutorial/Main.java
@@ -5,10 +5,6 @@ import io.evitadb.api.requestResponse.schema.Cardinality;
 import io.evitadb.driver.EvitaClient;
 import io.evitadb.driver.config.EvitaClientConfiguration;
 
-import java.math.BigDecimal;
-import java.util.Currency;
-import java.util.Locale;
-
 /**
  * This example class shows how to create evitaDB client and connect to the server.
  * It lists all catalogs available on the server and closes the connection.
@@ -31,55 +27,55 @@ public class Main {
         // define new catalog
         evita.defineCatalog("evita-tutorial")
                 .withDescription("This is a tutorial catalog.")
+                // define brand schema
+                .withEntitySchema(
+                        "Brand",
+                        whichIs -> whichIs.withDescription("A manufacturer of products.")
+                                .withAttribute(
+                                        "name", String.class,
+                                        thatIs -> thatIs.localized().filterable().sortable()
+                                )
+                )
+                // define category schema
+                .withEntitySchema(
+                        "Category",
+                        whichIs -> whichIs.withDescription("A category of products.")
+                                .withAttribute(
+                                        "name", String.class,
+                                        thatIs -> thatIs.localized().filterable().sortable()
+                                )
+                                .withHierarchy()
+                )
+                // define product schema
+                .withEntitySchema(
+                        "Product",
+                        whichIs -> whichIs.withDescription("A product in inventory.")
+                                .withAttribute(
+                                        "name", String.class,
+                                        thatIs -> thatIs.localized().filterable().sortable()
+                                )
+                                .withAttribute(
+                                        "cores", Integer.class,
+                                        thatIs -> thatIs.withDescription("Number of CPU cores.")
+                                                .filterable()
+                                )
+                                .withAttribute(
+                                        "graphics", String.class,
+                                        thatIs -> thatIs.withDescription("Graphics card.")
+                                                .filterable()
+                                )
+                                .withPrice()
+                                .withReferenceToEntity(
+                                        "brand", "Brand", Cardinality.EXACTLY_ONE,
+                                        thatIs -> thatIs.indexed()
+                                )
+                                .withReferenceToEntity(
+                                        "categories", "Category", Cardinality.ZERO_OR_MORE,
+                                        thatIs -> thatIs.indexed()
+                                )
+                )
+                // and now push all the definitions (mutations) to the server
                 .updateViaNewSession(evita);
-        // define schema by creating the entities with data
-        evita.updateCatalog(
-                "evita-tutorial",
-                session -> {
-                    // let's create a Lenovo brand first
-                    session.createNewEntity("Brand", 1)
-                            .setAttribute("name", Locale.ENGLISH, "Lenovo")
-                            .upsertVia(session);
-
-                    // now create an example category tree
-                    session.createNewEntity("Category", 10)
-                            .setAttribute("name", Locale.ENGLISH, "Electronics")
-                            .upsertVia(session);
-
-                    session.createNewEntity("Category", 11)
-                            .setAttribute("name", Locale.ENGLISH, "Laptops")
-                            // laptops will be a child category of electronics
-                            .setParent(10)
-                            .upsertVia(session);
-
-                    // finally, create a product
-                    session.createNewEntity("Product")
-                            // with a few attributes
-                            .setAttribute("name", Locale.ENGLISH, "ThinkPad P15 Gen 1")
-                            .setAttribute("cores", 8)
-                            .setAttribute("graphics", "NVIDIA Quadro RTX 4000 with Max-Q Design")
-                            // and price for sale
-                            .setPrice(
-                                    1, "basic",
-                                    Currency.getInstance("USD"),
-                                    new BigDecimal("1420"), new BigDecimal("20"), new BigDecimal("1704"),
-                                    true
-                            )
-                            // link it to the manufacturer
-                            .setReference(
-                                    "brand", "Brand",
-                                    Cardinality.EXACTLY_ONE,
-                                    1
-                            )
-                            // and to the laptop category
-                            .setReference(
-                                    "categories", "Category",
-                                    Cardinality.ZERO_OR_MORE,
-                                    11
-                            )
-                            .upsertVia(session);
-                }
-        );
         // close the connection
         evita.close();
         System.out.println("evitaDB connection closed");
