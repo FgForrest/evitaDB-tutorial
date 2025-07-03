f7fb6d45809014f7094413034b7129c0152f03a3
Jan Novotný <novotnaci@gmail.com>
2025-07-03 08:50:10 +0200

    chore: upgrade to 2025.4.X

--- src/main/java/io/evitadb/tutorial/Main.java
+++ src/main/java/io/evitadb/tutorial/Main.java
@@ -20,10 +20,9 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         final EvitaContract evita = new EvitaClient(
-                EvitaClientConfiguration.builder()
-                        .host("localhost")
-                        .port(5556)
-                        .build()
+            EvitaClientConfiguration.builder()
+                .host("localhost")
+                .build()
         );
 
         System.out.println("evitaDB connected ... defining schema");
@@ -31,21 +30,22 @@ public class Main {
         evita.deleteCatalogIfExists("evita-tutorial");
         // define new catalog
         evita.defineCatalog("evita-tutorial")
-                .withDescription("This is a tutorial catalog.")
-                .updateViaNewSession(evita);
+            .withDescription("This is a tutorial catalog.")
+            .updateViaNewSession(evita);
+
+        System.out.println("- catalog `evita-tutorial` created, now defining entity schemas");
 
         // define entity schemas by Java interfaces
         evita.updateCatalog(
-                "evita-tutorial",
-                session -> {
-                    session.defineEntitySchemaFromModelClass(Brand.class);
-                    session.defineEntitySchemaFromModelClass(Category.class);
-                    session.defineEntitySchemaFromModelClass(Product.class);
-
-                    // TODO: temporary workaround until evitaLab supports warm-up state
-                    session.goLiveAndClose();
-                }
+            "evita-tutorial",
+            session -> {
+                session.defineEntitySchemaFromModelClass(Brand.class);
+                session.defineEntitySchemaFromModelClass(Category.class);
+                session.defineEntitySchemaFromModelClass(Product.class);
+            }
         );
+
+        System.out.println("- entity schemas defined, now creating some data");
 
         // create some data via custom contracts
         final int productId = setUpNewProduct(evita);
@@ -66,99 +66,101 @@ public class Main {
 
     private static int setUpNewProduct(EvitaContract evita) {
         return evita.updateCatalog(
-                "evita-tutorial",
-                session -> {
-                    System.out.print("evitaDB ... creating Apple brand ...");
-                    // create a new brand
-                    final EntityReference appleBrandRef = session.createNewEntity(BrandEditor.class)
-                            .setName("Apple", Locale.ENGLISH)
-                            .upsertVia(session);
-                    System.out.println(" ok.");
+            "evita-tutorial",
+            session -> {
+                System.out.print("- creating Apple brand ...");
+                // create a new brand
+                final EntityReference appleBrandRef = session.createNewEntity(BrandEditor.class)
+                    .setName("Apple", Locale.ENGLISH)
+                    .upsertVia(session);
+                System.out.println(" ok.");
 
-                    System.out.print("evitaDB ... creating Cell phones category ...");
-                    // create a new category
-                    final EntityReference cellPhonesRef = session.createNewEntity(CategoryEditor.class)
-                            .setName("Cell phones", Locale.ENGLISH)
-                            .upsertVia(session);
-                    System.out.println(" ok.");
+                System.out.print("- creating Cell phones category ...");
+                // create a new category
+                final EntityReference cellPhonesRef = session.createNewEntity(CategoryEditor.class)
+                    .setName("Cell phones", Locale.ENGLISH)
+                    .upsertVia(session);
+                System.out.println(" ok.");
 
-                    System.out.print("evitaDB ... creating iPhone 12 product ...");
-                    // create a new product linked to the brand and category
-                    final EntityReference productRef = session.createNewEntity(ProductEditor.class)
-                            .setName("iPhone 12", Locale.ENGLISH)
-                            .setCores(6)
-                            .setGraphics("A14 Bionic")
-                            .setBrandId(appleBrandRef.getPrimaryKey())
-                            .addCategoryId(cellPhonesRef.getPrimaryKey())
-                            .upsertVia(session);
-                    System.out.println(" ok.");
+                System.out.print("- creating iPhone 12 product ...");
+                // create a new product linked to the brand and category
+                final EntityReference productRef = session.createNewEntity(ProductEditor.class)
+                    .setName("iPhone 12", Locale.ENGLISH)
+                    .setCores(6)
+                    .setGraphics("A14 Bionic")
+                    .setBrandId(appleBrandRef.getPrimaryKey())
+                    .addCategoryId(cellPhonesRef.getPrimaryKey())
+                    .upsertVia(session);
+                System.out.println(" ok.");
 
-                    return productRef.getPrimaryKey();
-                }
+                return productRef.getPrimaryKey();
+            }
         );
     }
 
     private static void readProductAndPrintToConsole(EvitaContract evita, int productId) {
-    evita.queryCatalog(
+        System.out.println("- reading product with id " + productId + " ...");
+
+        evita.queryCatalog(
             "evita-tutorial",
             session -> {
                 final Product product = session.queryOne(
-                                query(
-                                        filterBy(
-                                                entityPrimaryKeyInSet(productId),
-                                                entityLocaleEquals(Locale.ENGLISH)
-                                        ),
-                                        require(
-                                                entityFetch(
-                                                        attributeContentAll(),
-                                                        referenceContent(
-                                                                Product.REFERENCE_BRAND,
-                                                                entityFetch(attributeContentAll())
-                                                        ),
-                                                        referenceContent(
-                                                                Product.REFERENCE_CATEGORIES,
-                                                                entityFetch(attributeContentAll())
-                                                        )
-                                                )
-                                        )
-                                ),
-                                Product.class
-                        )
-                        .orElseThrow(
-                                () -> new IllegalStateException("Product with id " + productId + " not found.")
-                        );
+                        query(
+                            filterBy(
+                                entityPrimaryKeyInSet(productId),
+                                entityLocaleEquals(Locale.ENGLISH)
+                            ),
+                            require(
+                                entityFetch(
+                                    attributeContentAll(),
+                                    referenceContent(
+                                        Product.REFERENCE_BRAND,
+                                        entityFetch(attributeContentAll())
+                                    ),
+                                    referenceContent(
+                                        Product.REFERENCE_CATEGORIES,
+                                        entityFetch(attributeContentAll())
+                                    )
+                                )
+                            )
+                        ),
+                        Product.class
+                    )
+                    .orElseThrow(
+                        () -> new IllegalStateException("Product with id " + productId + " not found.")
+                    );
 
-                System.out.println("Product name: " + product.getName());
-                System.out.println("Product cores: " + product.getCores());
-                System.out.println("Product graphics: " + product.getGraphics());
-                System.out.println("Product brand: " + product.getBrand().getName());
+                System.out.println("\tProduct name: " + product.getName());
+                System.out.println("\tProduct cores: " + product.getCores());
+                System.out.println("\tProduct graphics: " + product.getGraphics());
+                System.out.println("\tProduct brand: " + product.getBrand().getName());
                 System.out.println(
-                        "Product categories: " +
-                                product.getCategories()
-                                        .stream()
-                                        .map(Category::getName)
-                                        .reduce((a, b) -> a + ", " + b)
-                                        .orElse("<none>")
+                    "\tProduct categories: " +
+                        product.getCategories()
+                            .stream()
+                            .map(Category::getName)
+                            .reduce((a, b) -> a + ", " + b)
+                            .orElse("<none>")
                 );
             }
-    );
+        );
     }
 
     private static void updateExistingProduct(EvitaContract evita, int productId) {
         evita.updateCatalog(
-                "evita-tutorial",
-                session -> {
-                    System.out.print("evitaDB ... updating iPhone 12 product to Pro ...");
-                    session.getEntity(
-                                    Product.class, productId, entityFetchAllContent()
-                            )
-                            .orElseThrow()
-                            .openForWrite()
-                            .setName("iPhone 12 Pro", Locale.ENGLISH)
-                            .setCores(8)
-                            .upsertVia(session);
-                    System.out.println(" ok.");
-                }
+            "evita-tutorial",
+            session -> {
+                System.out.print("- updating iPhone 12 product to Pro ...");
+                session.getEntity(
+                        Product.class, productId, entityFetchAllContent()
+                    )
+                    .orElseThrow()
+                    .openForWrite()
+                    .setName("iPhone 12 Pro", Locale.ENGLISH)
+                    .setCores(8)
+                    .upsertVia(session);
+                System.out.println(" ok.");
+            }
         );
     }
 
